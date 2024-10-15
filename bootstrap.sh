6337e953580f09e22a7fdda153eeae1fda0247ef
Kulib <kulibchenkovalera@gmail.com>
2024-10-15 19:25:56 +0300

    third commit

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@ kind create cluster --config cluster.yml
 kubectl taint nodes kind-worker app=mysql:NoSchedule
 kubectl taint nodes kind-worker2 app=mysql:NoSchedule
 
-helm install todoapp-release ./infrastructure/helm-charts/todoapp/
+helm install todoapp-release ./infrastructure/helm-chart/todoapp/
